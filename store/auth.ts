746a1509c8456b9d9964c592fad739e4798b6f64
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-04-15 22:56:10 +0600

    code is ongoing

---
+++ store/auth.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+
+interface authProvider{
+ name:string,   
+ email:string,   
+ password:string,
+ answer:string
+}
+
+// const initialState:authProvider{
+//  user:authProvider,
+
+// }
+
+
